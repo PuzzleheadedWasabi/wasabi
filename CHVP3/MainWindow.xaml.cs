ce30d79531e418350db0c5a1dab83193d61b9bf3
PuzzleHeadedWasabi <PuzzleHeadedWasabi@gmail.com>
2019-12-02 23:59:31 +1100

    Controlling VLC working

--- CHVP3/MainWindow.xaml.cs
+++ CHVP3/MainWindow.xaml.cs
@@ -14,6 +14,14 @@ using System.Windows.Shapes;
 using CHVP3.Properties;
 using System.Collections.ObjectModel;
 using System.Threading;
+using System.Diagnostics;
+
+using System.Management;
+using System.IO;
+
+using Microsoft.Win32;
+using System.Net;
+using System.Net.Sockets;
 
 namespace CHVP3
 {
@@ -30,12 +38,191 @@ namespace CHVP3
             DataContext = LogEntries = new ObservableCollection<LogEntry>();
 
             AddEntry(new LogEntry("Started CHVP"));
+            Debug.WriteLine("BOO");
 
-            Timer = new Timer(x => AddEntry(GetRandomEntry()), null, 5000, 1000);
+            Timer = new Timer(x => CheckIfVLCRunning(), null, 0, 1000);
         }
 
+        private Process controllingProcess = null;
         private Timer Timer;
         private Random random = new Random();
+
+        // Returns the file, or null if no file
+        private string[] GetVLCFile(Process process)
+        {
+            string cli = GetCommandLine(process);
+
+            // First get the path to the vlc executable
+            string[] chunks = cli.Split('\"');
+
+            if (chunks.Length <= 1) return null;
+
+            string pathToVLC = chunks[1];
+            Log("Path to VLC: " + pathToVLC);
+
+            string check = "--started-from-file";
+            int index = cli.IndexOf(check);
+
+            if (index == -1) return null;
+
+            cli = cli.Substring(index + check.Length);
+            cli = cli.TrimStart();
+            Debug.WriteLine(cli);
+
+            chunks = cli.Split('\"');
+
+            if (chunks.Length <= 1) return null;
+
+            // The first index should be empty or an equals sign or something, cbs checking
+            string filePath = chunks[1];
+            Debug.WriteLine(filePath);
+
+            bool exists = File.Exists(filePath);
+            Debug.WriteLine(exists);
+
+            if (!exists) return null;
+
+            return new string[] { pathToVLC, filePath };
+        }
+
+        //private string GetVLCPath()
+        //{
+
+        //    string InstallPath = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\VideoLAN\VLC", "InstallDir", null);
+
+        //    Log(InstallPath);
+
+        //    //try
+        //    //{
+        //    //    using (RegistryKey key = Registry.LocalMachine.OpenSubKey("SOFTWARE"))
+        //    //    {
+        //    //        if (key != null)
+        //    //        {
+        //    //            Log(key.GetSubKeyNames().Length + "");
+        //    //            Log(key.GetSubKeyNames()[0]);
+
+        //    //            Log("aaaa");
+
+        //    //            Object o = key.GetValue("InstallDir");
+        //    //            if (o != null)
+        //    //            {
+        //    //                Log("Found VLC Path: " + o.ToString());
+        //    //                return o.ToString();
+        //    //            }
+        //    //        }
+        //    //    }
+        //    //}
+
+
+        //    //catch (Exception ex)  //just for demonstration...it's always best to handle specific exceptions
+        //    //{
+        //    //    Log("Exception when getting VLC Path");
+        //    //}
+
+        //    //Log("Failed to get VLC Path");
+
+        //    return null;
+
+        //}
+
+
+        private void ControlVLC(Process process, string[] vlcDetails)
+        {
+            process.Kill();
+
+            IPEndPoint socketAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 54174);
+
+            //string args = $"--intf rc play \"{vlcDetails[1]}\"";
+            string args = "--fullscreen -I rc --rc-host " + socketAddress.ToString() + $" \"{vlcDetails[1]}\"";
+            Log("Running command: " + args);
+            Debug.WriteLine(args);
+
+            Process controllingProcess = new Process();
+            controllingProcess.StartInfo.UseShellExecute = false;
+            controllingProcess.StartInfo.FileName = vlcDetails[0];
+            controllingProcess.StartInfo.Arguments = args;
+            controllingProcess.Start();
+
+            Log("PID of controllingProcess is " + controllingProcess.Id);
+
+            try
+            {
+                Socket vlcRcSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                vlcRcSocket.Connect(socketAddress);
+                // start another thread to look for responses and display them
+                //Task listener = Task.Factory.StartNew(() => VLCInterface.Receive(vlcRcSocket));
+
+                Log("Connected to VLC process!");
+
+                Thread.Sleep(1500);
+                VLCInterface.Send(vlcRcSocket, "seek 20");
+
+                Thread.Sleep(1500);
+                VLCInterface.Send(vlcRcSocket, "pause");
+
+                Thread.Sleep(1500);
+                VLCInterface.Send(vlcRcSocket, "pause");
+                VLCInterface.Send(vlcRcSocket, "get_time");
+
+                Thread.Sleep(1500);
+
+                VLCInterface.Send(vlcRcSocket, "quit"); // close vlc rc interface and disconnect
+                vlcRcSocket.Disconnect(false);
+            }
+            finally
+            {
+                controllingProcess.Kill();
+            }
+
+            //Thread.Sleep(2000);
+            //controllingProcess.StandardInput.WriteLine("pause");
+
+            controllingProcess.WaitForExit();
+
+            Log("Process has exited!");
+            controllingProcess = null;
+
+        }
+
+        private void CheckIfVLCRunning()
+        {
+            Log("CheckIfVLCRunning");
+
+            Process vlc = GetVLCProcess();
+            if (vlc == null) return;
+
+            string[] vlcDetails = GetVLCFile(vlc);
+            if (vlcDetails == null) return;
+
+            ControlVLC(vlc, vlcDetails);
+
+        }
+
+        private Process GetVLCProcess()
+        {
+
+            // If we are already controlling a process, then just exit
+            if (controllingProcess != null) return null;
+
+            Process[] processes = Process.GetProcesses();
+
+            // Only support single VLC process
+            foreach (Process process in processes)
+            {
+                if (!process.ProcessName.ToLower().Equals("vlc")) continue;
+                String cli = GetCommandLine(process);
+                if (cli.ToLower().Contains("--intf rc")) continue;
+                Debug.WriteLine("Found VLC, ID: {0}, CLI: {1}", process.Id, GetCommandLine(process));
+                return process;
+            }
+
+            return null;
+        }
+
+        private void Log(string message)
+        {
+            Dispatcher.BeginInvoke((Action)(() => LogEntries.Add(new LogEntry(message))));
+        }
 
         private void AddEntry(LogEntry entry)
         {
@@ -53,9 +240,20 @@ namespace CHVP3
             );
         }
 
+        private static string GetCommandLine(Process process)
+        {
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + process.Id))
+            using (ManagementObjectCollection objects = searcher.Get())
+            {
+                return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
+            }
+
+        }
 
 
 
     }
+
+
 
 }
---
+++ CHVP3/VLCInterface.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHVP3
+{
+    class VLCInterface
+    {
+
+        static void boo()
+        {
+            IPEndPoint socketAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 54174);
+            var vlcServerProcess = Process.Start(@"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe", "-I rc --rc-host " + socketAddress.ToString());
+
+            try
+            {
+                Socket vlcRcSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                vlcRcSocket.Connect(socketAddress);
+                // start another thread to look for responses and display them
+                Task listener = Task.Factory.StartNew(() => Receive(vlcRcSocket));
+
+
+                while (true)
+                {
+                    string command = Console.ReadLine();
+                    if (command.Equals("quit")) break;
+                    Send(vlcRcSocket, command);
+                }
+
+                Send(vlcRcSocket, "quit"); // close vlc rc interface and disconnect
+                vlcRcSocket.Disconnect(false);
+            }
+            finally
+            {
+                vlcServerProcess.Kill();
+            }
+        }
+
+        public static void Send(Socket socket, string command)
+        {
+            // send command to vlc socket, note \n is important
+            byte[] commandData = UTF8Encoding.UTF8.GetBytes(String.Format("{0}\n", command));
+            int sent = socket.Send(commandData);
+        }
+
+        public static void Receive(Socket socket)
+        {
+            do
+            {
+                if (socket.Connected == false) break;
+                // check if there is any data
+                bool haveData = socket.Poll(1000000, SelectMode.SelectRead);
+
+                if (haveData == false) continue;
+                byte[] buffer = new byte[socket.ReceiveBufferSize];
+                using (MemoryStream mem = new MemoryStream())
+                {
+                    while (haveData)
+                    {
+                        int received = socket.Receive(buffer);
+                        mem.Write(buffer, 0, received);
+                        haveData = socket.Poll(1000000, SelectMode.SelectRead);
+                    }
+
+                    Debug.WriteLine(Encoding.UTF8.GetString(mem.ToArray()));
+                }
+
+            } while (true);
+        }
+
+    }
+}

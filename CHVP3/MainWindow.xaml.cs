1ef4b99e7970c63daf028c4feb88229933cdb983
PuzzleHeadedWasabi <PuzzleHeadedWasabi@gmail.com>
2019-12-02 21:25:24 +1100

    Log viewer

--- CHVP3/MainWindow.xaml.cs
+++ CHVP3/MainWindow.xaml.cs
@@ -12,17 +12,104 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CHVP3.Properties;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Threading;
 
 namespace CHVP3
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+
+    public partial class LogViewer : Window
     {
-        public MainWindow()
+        private string TestData = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+        private List<string> words;
+        private int maxword;
+        private int index;
+
+        public ObservableCollection<LogEntry> LogEntries { get; set; }
+
+        public LogViewer()
         {
             InitializeComponent();
+
+            random = new Random();
+            words = TestData.Split(' ').ToList();
+            maxword = words.Count - 1;
+
+            DataContext = LogEntries = new ObservableCollection<LogEntry>();
+            Enumerable.Range(0, 5)
+                      .ToList()
+                      .ForEach(x => LogEntries.Add(GetRandomEntry()));
+
+            Timer = new Timer(x => AddRandomEntry(), null, 5000, 1000);
+        }
+
+        private System.Threading.Timer Timer;
+        private System.Random random;
+        private void AddRandomEntry()
+        {
+            Dispatcher.BeginInvoke((Action)(() => LogEntries.Add(GetRandomEntry())));
+        }
+
+        private LogEntry GetRandomEntry()
+        {
+            if (random.Next(1, 10) > 1)
+            {
+                return new LogEntry()
+                {
+                    Index = index++,
+                    DateTime = DateTime.Now,
+                    Message = string.Join(" ", Enumerable.Range(5, random.Next(10, 50))
+                                                         .Select(x => words[random.Next(0, maxword)])),
+                };
+            }
+
+            return new CollapsibleLogEntry()
+            {
+                Index = index++,
+                DateTime = DateTime.Now,
+                Message = string.Join(" ", Enumerable.Range(5, random.Next(10, 50))
+                                                        .Select(x => words[random.Next(0, maxword)])),
+                Contents = Enumerable.Range(5, random.Next(5, 10))
+                                                .Select(i => GetRandomEntry())
+                                                .ToList()
+            };
+
+        }
+
+
+
+
+    }
+
+
+    public class LogEntry : PropertyChangedBase
+    {
+        public DateTime DateTime { get; set; }
+
+        public int Index { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    public class CollapsibleLogEntry : LogEntry
+    {
+        public List<LogEntry> Contents { get; set; }
+    }
+
+    public class PropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            Application.Current.Dispatcher.BeginInvoke((Action)(() =>
+            {
+                PropertyChangedEventHandler handler = PropertyChanged;
+                if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+            }));
         }
     }
+
 }
